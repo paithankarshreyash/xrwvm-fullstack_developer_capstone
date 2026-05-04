859e2808571b289106621c18c59d9cfa9091b693
paithankarshreyash <paithankarshreyash@gmail.com>
2026-05-04 18:20:59 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path(route='get_dealerships', view=views.get_dealerships, name='get_dealerships'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='add_review', view=views.add_review, name='add_review'),
     path(route='get_dealerships/<str:state>', view=views.get_dealerships, name='get_dealerships_by_state'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
